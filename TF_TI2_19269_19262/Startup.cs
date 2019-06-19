ef91650cee91feb1c0563ffc5c788516a4bbd38c
Telmo Faria <aluno19269@ipt.pt>
2019-06-19 14:30:25 +0100

    Users e roles
    Users criados bem como as roles no controller das series

--- TF_TI2_19269_19262/Startup.cs
+++ TF_TI2_19269_19262/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using Owin;
 using TF_TI2_19269_19262.Models;
+using System.Linq;
 
 namespace TF_TI2_19269_19262
 {
@@ -26,7 +27,7 @@ namespace TF_TI2_19269_19262
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
 
-            // criar a Role 'Agente'
+            // criar a Role 'Utilizador'
             if (!roleManager.RoleExists("Utilizador"))
             {
                 // não existe a 'role'
@@ -36,21 +37,69 @@ namespace TF_TI2_19269_19262
                 roleManager.Create(role);
             }
 
+            if (!roleManager.RoleExists("Moderador"))
+            {
+                // não existe a 'role'
+                // então, criar essa role
+                var role = new IdentityRole();
+                role.Name = "Moderador";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Administrador"))
+            {
+                // não existe a 'role'
+                // então, criar essa role
+                var role = new IdentityRole();
+                role.Name = "Administrador";
+                roleManager.Create(role);
+            }
 
 
             // criar um utilizador 'Utilizador'
             var user = new ApplicationUser();
             user.UserName = "telmo@teste.pt";
             user.Email = "telmo@teste.pt";
-            user.Nome = "Telmo Faria";
             string userPWD = "123_Asd";
             var chkUser = userManager.Create(user, userPWD);
+            var getall = db.Utilizadores.ToList();
 
-            //Adicionar o Utilizador à respetiva Role-Agente-
+            //Adicionar o Utilizador à respetiva Role-Utilizador-
             if (chkUser.Succeeded)
             {
                 var result1 = userManager.AddToRole(user.Id, "Utilizador");
             }
+
+            // criar um utilizador 'Utilizador'
+            user = new ApplicationUser();
+            user.UserName = "joao@teste.pt";
+            user.Email = "joao@teste.pt";
+            userPWD = "123_Asd";
+            chkUser = userManager.Create(user, userPWD);
+            getall = db.Utilizadores.ToList();
+
+            //Adicionar o Utilizador à respetiva Role-Utilizador-
+            if (chkUser.Succeeded)
+            {
+                var result1 = userManager.AddToRole(user.Id, "Utilizador");
+            }
+
+            user = new ApplicationUser();
+            user.UserName = "admin@teste.pt";
+            user.Email = "admin@teste.pt";
+            userPWD = "123_Asd";
+            chkUser = userManager.Create(user, userPWD);
+
+            getall = db.Utilizadores.ToList();
+
+            //Adicionar o Utilizador à respetiva Role-Utilizador-
+            if (chkUser.Succeeded)
+            {
+                var result1 = userManager.AddToRole(user.Id, "Administrador");
+            }
+
+
+
         }
 
         // https://code.msdn.microsoft.com/ASPNET-MVC-5-Security-And-44cbdb97
