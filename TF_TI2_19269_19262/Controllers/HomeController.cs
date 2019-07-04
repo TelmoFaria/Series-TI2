380a31ca5ee3b14ca0b7704e1d80bfef3fb56b06
Joao Alves <aluno19262@ipt.pt>
2019-07-04 14:55:29 +0100

    error redirect

--- TF_TI2_19269_19262/Controllers/HomeController.cs
+++ TF_TI2_19269_19262/Controllers/HomeController.cs
@@ -10,21 +10,22 @@ namespace TF_TI2_19269_19262.Controllers
             return View();
         }
 
-        [HttpGet]
-        [Authorize]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
 
-            return View();
-        }
+        //[HttpGet]
+        //[Authorize]
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your app description page.";
 
-        [HttpGet]
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //    return View();
+        //}
 
-            return View();
-        }
+        //[HttpGet]
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
